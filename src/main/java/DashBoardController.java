62fec1c1cbab85c9967d33a19a539a95a6710439
tuan <tuanbigboss1@gmail.com>
2021-10-02 23:16:22 +0700

    update event lan 1, sua lai 1 it giao dien can setup lai project de run

--- src/main/java/DashBoardController.java
+++ src/main/java/DashBoardController.java
@@ -1,15 +1,45 @@
+//package demo.demo;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
+public class DashBoardController {
+    //private textToSpeech speech = new textToSpeech();
+    @FXML
+    private TextField textSearch;
+    @FXML
+    private ListView<String> listSearch;
+    @FXML
+    private void renderListSearch() {
+        ObservableList<String> items = FXCollections.observableArrayList();
+        items.addAll("String", "Application", "Demo", "Render");
+        listSearch.setItems(items);
+    }
 
-import java.net.URL;
-import java.util.ResourceBundle;
+    @FXML
+    private Label englishWord;
+    @FXML
+    private Label vietnameseWord;
+    @FXML
+    private void renderEnglishToVietnamese(MouseEvent event) {
+        String english = listSearch.getSelectionModel().getSelectedItem();
+        int index = listSearch.getSelectionModel().getSelectedIndex();;
+        englishWord.setText(english);
+        vietnameseWord.setText("tu dich o vi tri co index = " + Integer.toString(index));
+    }
 
-public class DashBoardController  {
-
+    @FXML
+    private Button speakEN;
+    /*@FXML
+    private void speckSpeech() {
+        String text = englishWord.getText();
+        speech.speak(text);
+    }
+    */
 }
